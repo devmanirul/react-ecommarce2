b6be7da656a64625b36b148a17ba78627d94f1e0
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2025-04-27 21:23:15 +0600

    update s

--- src/components/CommonComponent/Navbar/index.jsx
+++ src/components/CommonComponent/Navbar/index.jsx
@@ -1,4 +1,16 @@
 const Navbar = () => {
-  return <div className="text-3xl font-bold">Navbar</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-neutral">Navbar</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
 };
 export default Navbar;
